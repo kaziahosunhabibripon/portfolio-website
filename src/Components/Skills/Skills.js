d82b888c6f6acb497ee0dd4e56dd016295b53f17
Ripon <habibkazi92@gmail.com>
2021-05-02 06:07:31 +0800

    skills

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import Resume from '../../Images/Resume.pdf';
 import './Skills.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import bootstrap from '../../Images/bootstrap.png';
+import html from '../../Images/html.jpg';
+import css from '../../Images/css.jpg';
+import js from '../../Images/js.jpg';
 const Skills = () => {
     return (
         <section className="row m-0 p-0">
             <div className="col-md-1">
-
             </div>
             <div className="col-md-4">
                 <Card style={{ width: '30rem' }} className="card-content shadow-lg rounded">
                     <Card.Body>
                         <Card.Title className="text-center"> <h1 className="text-info">About Me</h1></Card.Title>
                         <Card.Text className="text-white">
-                            Hi I am <span className="text-warning"> Kazi Ahosun Habib Ripon</span>. I am a web developer can do responsive web design and development.
-                            I have a vast experience in HTML, CSS, Bootstrap,Javascript and React.
-                            Also well experites in MERN stack developer.
+                            Hi I am <span className="text-warning"> Kazi Ahosun Habib Ripon</span>. I am a MERN stack web developer. I have vast experience to make all type of responsive web page design.
+                            I have a well expertise in experience in HTML, CSS, Bootstrap,Javascript and React.
                         </Card.Text>
                     </Card.Body>
                     <div className="row d-flex pl-3">
@@ -29,28 +33,28 @@ const Skills = () => {
                         </ul>
                     </div>
                     <Card.Body className="text-center">
-                        <Card.Link href={Resume} downloaded="true"><button className="btn btn-info text-white text-uppercase">Download Resume</button></Card.Link>
+                        <Card.Link href={Resume} download="true" target="_blank"><button className="btn btn-info text-white"><FontAwesomeIcon icon={faDownload} className="text-white" /> Download Resume </button></Card.Link>
                     </Card.Body>
                 </Card>
             </div>
-            <div className="col-md-7 d-flex m-0 p-0">
-                <div className="col-md-4" style={{ border: "1px solid red" }}>
-                    <h1>This is</h1>
-                </div>
-                <div className="col-md-4" style={{ border: "1px solid red" }}>
-                    <h1>This is</h1>
-                    <h1>This is</h1>
-                </div>
-                <div className="col-md-4" >
-                    <div className="row">
-                        <div className="col-md-12">
-
-                        </div>
+            <div className="col-md-6 shadow-lg rounded">
+            <h1 className="text-center skill-heading">My Skills</h1>
+                <div className="row m-0 p-0">
+                    <div className="col-md-4 Skills">
+                        <img src={html} alt="HTML" /> 
                     </div>
-                    <h1>This is</h1>
-                    <h1>This is</h1>
+                    <div className="col-md-4 Skills">
+                        <img src={css} alt="css"  />  
+                    </div>
+                    <div className="col-md-4 Skills">
+                        <img src={bootstrap} alt="bootstrap"  />
+                    </div>
+                    <div className="col-md-4 Skills">
+                        <img src={js} alt="js"  />
+                    </div>
+                    
                 </div>
-            </div>
+            </div>    
         </section>
     );
 };
